0044b0161c70b2d3474e3911844f84229b1b9928
keroviv86 <92390457+keroviv86@users.noreply.github.com>
2022-02-02 12:59:44 -0600

    Update song.rb

--- lib/song.rb
+++ lib/song.rb
@@ -26,6 +26,7 @@ class Song
     def self.artist_count
         artist_count={}
         @@artists.each do |artist|
+#             check if artist is in the hash
             if artist_count[artist] 
                 artist_count[artist]+=1
             else 
@@ -50,8 +51,5 @@ class Song
           end
         end
         genre_count
-    
-        # with #tally
-        # @@genres.tally
       end
 end
